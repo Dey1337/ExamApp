bd4df000f97d51502fa51db93b127411c4ef6e6f
Adrian Mazilu <47982757+Dey1337@users.noreply.github.com>
2022-05-16 22:55:15 +0300

    ReadKey
    Learning how to use readkey , started working on game class

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp1
 {
     internal class Game
     {
+        public void Start()
+        {
+            WriteLine("The game is starting");
+
+            ConsoleKeyInfo keyPressed = ReadKey();
+            if (keyPressed.Key == ConsoleKey.Enter)
+            {
+                WriteLine("You pressed Enter");
+            }
+            else
+            {
+                WriteLine("You pressed another key");
+            }
+
+            WriteLine("Press any key to exit ...");
+            ReadKey(true);
+
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Game mygame = new Game();
+            mygame.Start();
         }
     }
 }
