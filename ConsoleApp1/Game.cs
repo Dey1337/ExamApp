50016aba934c97f8fb87fce53feb06789b6328ff
Adrian Mazilu <47982757+Dey1337@users.noreply.github.com>
2022-05-18 23:23:35 +0300

    Made a program with custom console menu
    I learned to make a custom console menu for a program I needed for a subject in uni. I will make this a savepoint because I will use this concept of console menu for my game.

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -7,25 +7,202 @@ using static System.Console;
 
 namespace ConsoleApp1
 {
-    internal class Game
+    class Game
     {
+        public static class PermutationsAndCombinations
+        {
+            public static long nCr(int n, int r)
+            {
+                // naive: return Factorial(n) / (Factorial(r) * Factorial(n - r));
+                return nPr(n, r) / Factorial(r);
+            }
+
+            public static long nPr(int n, int r)
+            {
+                // naive: return Factorial(n) / Factorial(n - r);
+                return FactorialDivision(n, n - r);
+            }
+
+            private static long FactorialDivision(int topFactorial, int divisorFactorial)
+            {
+                long result = 1;
+                for (int i = topFactorial; i > divisorFactorial; i--)
+                    result *= i;
+                return result;
+            }
+
+            public static long Factorial(int i)
+            {
+                if (i <= 1)
+                    return 1;
+                return i * Factorial(i - 1);
+            }
+        }
+
         public void Start()
         {
-            WriteLine("The game is starting");
-
-            ConsoleKeyInfo keyPressed = ReadKey();
-            if (keyPressed.Key == ConsoleKey.Enter)
-            {
-                WriteLine("You pressed Enter");
-            }
-            else
-            {
-                WriteLine("You pressed another key");
-            }
-
-            WriteLine("Press any key to exit ...");
-            ReadKey(true);
+            Title = "EASY EDS V1 - Mazilu Adrian 2203A";
+            RunMainMenu();
 
         }
+
+        private void RunMainMenu()
+        {
+            string prompt = @"
+                    ███████╗ █████╗ ███████╗██╗   ██╗    ███████╗██████╗ ███████╗    ██╗   ██╗ ██╗
+                    ██╔════╝██╔══██╗██╔════╝╚██╗ ██╔╝    ██╔════╝██╔══██╗██╔════╝    ██║   ██║███║
+                    █████╗  ███████║███████╗ ╚████╔╝     █████╗  ██║  ██║███████╗    ██║   ██║╚██║
+                    ██╔══╝  ██╔══██║╚════██║  ╚██╔╝      ██╔══╝  ██║  ██║╚════██║    ╚██╗ ██╔╝ ██║
+                    ███████╗██║  ██║███████║   ██║       ███████╗██████╔╝███████║     ╚████╔╝  ██║
+                    ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚══════╝╚═════╝ ╚══════╝      ╚═══╝   ╚═╝
+                    
+
+( Use ArrowKeys to select any option )";
+
+
+            string[] options = { "Combinari", "Aranjamente", "Permutari" };
+            StartMenu mainMenu = new StartMenu(prompt, options);
+            int selectedIndex = mainMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Combinari();
+                    break;
+                case 1:
+                     Aranjamente();
+                    break;
+                case 2:
+                    Permutari();
+                    break;
+
+            }
+
+        }
+
+        public void Combinari()
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.DarkYellow;
+            Write(@"Formula COMBINARI : n! / k!(n-k)!");
+            ResetColor();
+            WriteLine("\n");
+
+            Write(@"Se calculeaza COMBINARI de n luate cate k..
+
+
+Dati valori pentru n si k : 
+
+n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Write(@"k = ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            WriteLine();
+
+            ForegroundColor = ConsoleColor.DarkYellow;
+
+            Write("Pentru cazul dat : ");
+            ResetColor();
+            Write("Combinari de " + n + " luate cate " + k + " : " + n + "! / " + k + "!(" + n + " - " + k + ")! = ");
+            Write(PermutationsAndCombinations.nCr(n, k));
+
+
+
+            WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+            WriteLine(@"Press Tab to return to main menu or press anything else to exit...");
+            WriteLine();
+            ConsoleKey keyPressed;
+            ConsoleKeyInfo keyInfo = ReadKey(true);
+            keyPressed = keyInfo.Key;
+            if (keyPressed == ConsoleKey.Tab)
+            {
+                Clear();
+                RunMainMenu();
+            }
+
+        }
+        public void Aranjamente()
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.DarkYellow;
+            Write(@"Formula ARANJAMENTE : n! / (n-k)!");
+            ResetColor();
+            WriteLine("\n");
+
+            Write(@"Se calculeaza ARANJAMENTE de n luate cate k..
+
+
+Dati valori pentru n si k : 
+
+n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Write(@"k = ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            WriteLine();
+
+            ForegroundColor = ConsoleColor.DarkYellow;
+
+            Write("Pentru cazul dat : ");
+            ResetColor();
+            Write("Aranjamente de " + n + " luate cate " + k + " : " + n + "! / ( "+ n + " - " + k + ")! = ");
+            Write(PermutationsAndCombinations.nPr(n, k));
+
+
+
+            WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+            WriteLine(@"Press Tab to return to main menu or press anything else to exit...");
+            WriteLine();
+            ConsoleKey keyPressed;
+            ConsoleKeyInfo keyInfo = ReadKey(true);
+            keyPressed = keyInfo.Key;
+            if (keyPressed == ConsoleKey.Tab)
+            {
+                Clear();
+                RunMainMenu();
+            }
+        }
+
+        public void Permutari()
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.DarkYellow;
+            Write(@"Formula PERMUTARI de grad n : n!");
+            ResetColor();
+            WriteLine("\n");
+
+            Write(@"Se calculeaza PERMUTARI de grad n ..
+
+
+Dati o valoare pentru n : 
+
+n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            WriteLine();
+
+            ForegroundColor = ConsoleColor.DarkYellow;
+
+            Write("Pentru cazul dat : ");
+            ResetColor();
+            Write("Permutari de ordin " + n + " : " + n + "! = ");
+            Write(PermutationsAndCombinations.Factorial(n));
+
+
+
+            WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+            WriteLine(@"Press Tab to return to main menu or press anything else to exit...");
+            WriteLine();
+            ConsoleKey keyPressed;
+            ConsoleKeyInfo keyInfo = ReadKey(true);
+            keyPressed = keyInfo.Key;
+            if (keyPressed == ConsoleKey.Tab)
+            {
+                Clear();
+                RunMainMenu();
+            }
+        }
+
     }
 }
